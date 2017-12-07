5146bcc72205d501b02ab3bd097d221d0b8943ee
systemxgl <123218497@qq.com>
2017-12-07 16:09:19 +0800

    新增打印网页信息
    新增打印网页信息

--- printhelper.js
+++ printhelper.js
@@ -10,6 +10,9 @@ var printhelper = {
 	printContent:function(data,callback){
 		return utils.sendPost("/home/printcontent2",data,callback);
 	},
+	printHtmlContent:function(data,callback){
+		return utils.sendPost("/home/printhtmlcontent",data,callback);
+	},
 	getTaskState:function(data,callback){
 		return utils.sendPost("/home/getprinttaskstate",data,callback);
 	}
